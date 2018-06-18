31917394878a52c2d75b2700c3960ade1aa69b07
Michelle Olivieri <michelleo72@gmail.com>
2018-06-18 17:02:56 -0400

    login setup/test

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user.try(:authenticate, params[:password])
+      session[:user_id] = @user.id
+      # redirect_to user_path(@user)
+    else
+      # redirect_to login_path
+    end
+  end
+
+  def destroy
+    if logged_in?
+      session.destroy
+      redirect_to root_path
+    end
+  end
+
+end
