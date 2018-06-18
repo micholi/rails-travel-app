6d7a1e83d97f3abbcce8d3f7d46820245286caf1
Michelle Olivieri <michelleo72@gmail.com>
2018-06-18 12:29:24 -0400

    added data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: "mich@test.com", name: "micholi", password: "brooklyn")
+User.create(email: "timmycat@test.com", name: "Timmy", password: "catnip")
+User.create(email: "eric@test.com", name: "Eric", password: "yankees")
+
+City.create(name: "New York", country_id: 1)
+City.create(name: "London", country_id: 2)
+City.create(name: "Madrid", country_id: 3)
+City.create(name: "Tokyo", country_id: 4)
+
+Country.create(name: "USA")
+Country.create(name: "England")
+Country.create(name: "Spain")
+Country.create(name: "Japan")
+
+Trip.create(user_id: 1, city_id: 2, rating: 5, fave_attraction: "Trafalgar Square", comment: "One of my favorite cities ever! Hope to visit again someday.")
+Trip.create(user_id: 1, city_id: 3, rating: 3, fave_attraction: "Parque del Buen Retiro", comment: "Cool city. Would've rated higher if my wallet wasn't stolen. Watch out for pickpockets!")
+Trip.create(user_id: 2, city_id: 1, rating: 4, fave_attraction: "Rockefeller Center", comment: "Fun place to visit! But a little too crowded and fast-paced.")
+Trip.create(user_id: 3, city_id: 4, rating: 5, fave_attraction: "The Tokyo Skytree", comment: "Amazing! Highly recommend!")
