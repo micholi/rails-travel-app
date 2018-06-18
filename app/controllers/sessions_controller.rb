a22bcbdb082df29383d3fd6466966f49e9b99d54
Michelle Olivieri <michelleo72@gmail.com>
2018-06-18 17:47:37 -0400

    user page updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:email])
-    if @user.try(:authenticate, params[:password])
+    @user = User.find_by(email: params[:user][:email])
+    if @user && @user.try(:authenticate, params[:user][:password])
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
