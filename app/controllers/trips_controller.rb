e999b593f0cb88680e36b500b72914f7e522f18c
Michelle Olivieri <michelleo72@gmail.com>
2018-06-18 20:00:57 -0400

    building out trips controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -12,6 +12,17 @@ class TripsController < ApplicationController
   end
 
   def create
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      redirect_to user_trips_path(@user)
+    else
+      redirect_to new_trip_url
+    end
   end
-  
+
+  private
+  def trip_params
+    params.require(:trip).permit(:user_id, :city_id, :rating, :fave_attraction, :comment, :city_attributes[:name, :country, :country_id])
+  end
+
 end
