4d1e97d5651acc227a591e137e8070d87509b06c
Michelle Olivieri <michelleo72@gmail.com>
2018-06-19 15:04:11 -0400

    building out users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,28 @@
 class UsersController < ApplicationController
 
   def show
+    # update with helper method
     @user = User.find_by(id: session[:user_id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to signup_path
+    end
   end
+
+  private
+  def user_params
+    # need to add Omniauth
+    params.require(:user).permit(:email, :name, :password)
+  end
+
 end
