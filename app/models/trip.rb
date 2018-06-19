a4b8bb3aa973a9d1a2cdd9e9b21b8c1b3eed0baa
Michelle Olivieri <michelleo72@gmail.com>
2018-06-19 10:00:29 -0400

    update trip new

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,21 +2,24 @@ class Trip < ApplicationRecord
   belongs_to :user
   belongs_to :city
 
-  def city_name=(name)
-    self.city = City.find_or_create_by(name: name)
-  end
+  def city_attributes=(attributes)
 
-  def city_name
-    self.city.name if self.city
-    self.city_id = city.id
   end
+#  def city_name=(name)
+#    self.city = City.find_or_create_by(name: name)
+#  end
 
-  def country_name=(name)
-    self.country = Country.find_or_create_by(name: name)
-  end
+#  def city_name
+#    self.city.name if self.city
+  #  self.city_id = city.id
+#  end
 
-  def country_name
-    country.cities << city
-  end
+#  def country_name=(name)
+#    self.country = Country.find_or_create_by(name: name)
+#  end
+
+#  def country_name
+#    country.cities << city
+#  end
 
 end
