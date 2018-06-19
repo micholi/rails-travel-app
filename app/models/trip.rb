b5dc859adf9e5d3bd6a060c83d31409f5d5e198b
Michelle Olivieri <michelleo72@gmail.com>
2018-06-18 22:06:14 -0400

    troubleshooting trip form

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,13 @@
 class Trip < ApplicationRecord
   belongs_to :user
   belongs_to :city
+
+  def city_attributes=(city_attributes)
+    if !city_attributes[:name].empty?
+      city = City.find_or_create_by(name: city_attributes[:name])
+      if !city_attributes[:country].empty?
+        country = Country.find_or_create_by(name: city_attributes[:country])
+      end
+    end
+  end
 end
