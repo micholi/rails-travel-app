de2223c5836b31be2706683b1c28a651ea9d8460
Michelle Olivieri <michelleo72@gmail.com>
2018-06-18 20:30:42 -0400

    updated routes for testing

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 
   root 'welcome#home'
 
-  resources :users do
-    resources :trips
-  end
+  resources :users
+  resources :trips
+
 
   resources :cities
 #  resources :trips
