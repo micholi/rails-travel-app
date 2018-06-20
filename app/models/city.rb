668fe8c2fc68e8a7b9e1700170b1afd3e8d274c0
Michelle Olivieri <michelleo72@gmail.com>
2018-06-20 13:10:27 -0400

    added city validation

--- app/models/city.rb
+++ app/models/city.rb
@@ -4,6 +4,7 @@ class City < ApplicationRecord
   has_many :users, through: :trips
 
   validates :name, presence: true, uniqueness: true
+  validates :country_id, presence: true
 
   def city_country
     "#{self.name}, #{country.name}"
