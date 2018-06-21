b79b222a18d2f8c43d2c4ac74c02afe401ffb6e4
Michelle Olivieri <michelleo72@gmail.com>
2018-06-21 15:36:10 -0400

    revisions to user pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,8 @@ class UsersController < ApplicationController
   end
 
   def show
-     @user = User.find_by(id: params[:id])
-    #set_user
+    @user = User.find_by(id: params[:id])
+    # set_user
   end
 
   def new
@@ -38,7 +38,6 @@ class UsersController < ApplicationController
 
   private
   def set_user
-
     @user = User.find_by(id: session[:user_id])
   end
 
