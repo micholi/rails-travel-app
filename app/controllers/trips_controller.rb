0b657361cdb5110c543eaa7e16798edad5231de4
Michelle Olivieri <michelleo72@gmail.com>
2018-06-21 09:03:48 -0400

    added require_login method

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,4 +1,5 @@
 class TripsController < ApplicationController
+  before_action :require_login
 
   def index
     set_user
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,9 @@ module ApplicationHelper
       User.find_by(id: session[:user_id])
     end
   end
+
+  def require_login
+    return head(:forbidden) unless session.include :user_id
+  end
+
 end
