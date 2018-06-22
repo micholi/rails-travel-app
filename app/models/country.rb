ea07c7754c3ee03b9cceedcd8869e23f6c113a5a
Michelle Olivieri <michelleo72@gmail.com>
2018-06-21 22:36:37 -0400

    added attraction randomizer

--- app/models/country.rb
+++ app/models/country.rb
@@ -3,4 +3,12 @@ class Country < ApplicationRecord
   has_many :trips, through: :cities
 
   validates :name, presence:true, uniqueness: true
+
+  def random_attraction
+    attractions = []
+    self.trips.each do |trip|
+      attractions << trip.fave_attraction
+    end
+    attractions.sample
+  end
 end
