66708685f6dffe76c865e7de3e3917e88e85b259
Michelle Olivieri <michelleo72@gmail.com>
2018-06-22 16:27:53 -0400

    added flash, changed redirect

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -13,17 +13,14 @@ class TripsController < ApplicationController
 
   def new
     set_user
-    @trip = @user.trips.build
   end
 
   def create
-    #binding.pry
-    # @user = User.find_by(id: session[:user_id])
     set_user
-    # or @trip.user = current_user?
+    # or @trip.user == current_user?
     @trip = Trip.new(trip_params)
     if @trip.save
-      redirect_to user_path(@user)
+      redirect_to user_trips_path(@user), :flash => { :success => "You've successfully added this trip!"}
     else
       render :new
     end
@@ -33,7 +30,7 @@ class TripsController < ApplicationController
     set_user
     @trip = Trip.find(params[:id])
     if @trip.user != current_user
-      redirect_to user_trips_path(current_user), :flash => { :danger => "You may not edit another traveler's trip."}
+      redirect_to user_trips_path(@user), :flash => { :danger => "You may not edit another traveler's trip."}
     end
   end
 
@@ -42,7 +39,7 @@ class TripsController < ApplicationController
     @trip = @user.trips.find(params[:id])
     @trip.update(trip_params)
     if @trip.save
-      redirect_to user_path(@user), :flash => { success => "You've successfully updated this trip!"}
+      redirect_to user_trips_path(@user), :flash => { :success => "Your trip has been updated!"}
     else
       render :edit
     end
