1d869d5059e410855b3553e10a011d42a374fbb7
Michelle Olivieri <michelleo72@gmail.com>
2018-06-22 16:00:10 -0400

    added flash messages and new conditional

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,13 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:user][:email])
-    if @user && @user.try(:authenticate, params[:user][:password])
+    if !@user
+      redirect_to login_path, :flash => { :danger => "Sorry, we don't recognize that email."}
+    elsif @user && @user.try(:authenticate, params[:user][:password])
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      redirect_to login_path
+      redirect_to login_path, :flash => { :danger => "Incorrect password. Please try again."}
     end
   end
 
