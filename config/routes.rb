c4d8e41df304102c9d0fc94d01da6ec7047d5aed
Michelle Olivieri <michelleo72@gmail.com>
2018-06-30 19:34:57 -0400

    re-ordered

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  
   root 'welcome#home'
+
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  get '/auth/facebook/callback' => 'sessions#facebookcreate'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   resources :users do
     resources :trips
@@ -13,13 +20,5 @@ Rails.application.routes.draw do
   get '/trips/five_star' => 'trips#five_star', as: '/fivestar'
   get '/cities/most_visited' => 'cities#most_visited', as: '/most_visited'
   get '/users/most_trips' => 'users#most_trips', as: '/most_trips'
-
-  get '/signup' => 'users#new'
-  post '/signup' => 'users#create'
-
-  get '/login' => 'sessions#new'
-  get '/auth/facebook/callback' => 'sessions#facebookcreate'
-  post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
 
 end
