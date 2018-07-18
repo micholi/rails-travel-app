acc2cba08c7d0ee40bd3407e225ae3f86e4698dd
Michelle Olivieri <michelleo72@gmail.com>
2018-07-18 09:06:06 -0400

    moved load user trips from show to js file

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,1 +1,16 @@
+$(function() {
+  $(".js-load-trips").on("click", function() {
+    let userId = parseInt($(".js-load-trips").attr("data-user-id"))
+    $.get(`/users/${userId}/trips.json`, function(data) {
 
+      let trips = data;
+      let tripsList = "";
+      trips.forEach(function(trip) {
+        let tripId = trip.id;
+        let city = trip.city.name;
+
+      $("#my-trips").append(`<a href="/users/${userId}/trips/${tripId}">${city}</a><br>`);
+    })
+    })
+  })
+})
