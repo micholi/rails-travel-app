129376a0117641ab182d7b8d98e6357ecf16a971
Michelle Olivieri <michelleo72@gmail.com>
2018-07-18 13:35:17 -0400

    still working through hide/show div

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,8 +1,10 @@
 $(function() {
-  $(".js-load-trips").on("click", function() {
+  txt = document.getElementById("my-trips");
+  btn = document.getElementById("btn");
+
+  function appendToDiv() {
     let userId = parseInt($(".js-load-trips").attr("data-user-id"))
     $.get(`/users/${userId}/trips.json`, function(data) {
-
       let trips = data;
       let tripsList = "";
       trips.forEach(function(trip) {
@@ -10,7 +12,22 @@ $(function() {
         let city = trip.city.name;
 
       $("#my-trips").append(`<a href="/users/${userId}/trips/${tripId}">${city}</a><br>`);
-    })
-    })
+      btn.innerHTML = "Hide Trips"
+    });
+  });
+  }
+
+  $(".js-load-trips").on("click", function() {
+      if (btn.innerHTML === "See my trips") {
+        appendToDiv()
+      } else if (btn.innerHTML === "Hide Trips"){
+        $("div#my-trips").hide();
+        btn.innerHTML = "Show Trips"
+      } else if (btn.innerHTML === "Show Trips"){
+        $("div#my-trips").show()
+        btn.innerHTML = "Hide Trips"
+      }
   })
+
+
 })
