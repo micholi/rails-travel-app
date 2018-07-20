2077170a329cbf71c48bb702ebb000b295ce375a
Michelle Olivieri <michelleo72@gmail.com>
2018-07-20 12:53:20 -0400

    fixed show/hide trips that broke

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -4,7 +4,7 @@ function User(data) {
 
 User.prototype.formatTrips = function(userId) {
   let userTrips = this.trips
-  var tripsHtml = `<table>
+  let tripsHtml = `<br><br><table>
   <thead>
     <tr>
       <th>City</th>
@@ -16,28 +16,23 @@ User.prototype.formatTrips = function(userId) {
     let userTripId = trip.id;
     let city = trip.city.name;
     let fave = trip.fave_attraction;
-
-    //tripsHtml += `<a href="/users/${userId}/trips/${userTripId}">${city}</a><br>`
+    btn = document.getElementById("btn");
 
     tripsHtml +=`<tr class="border-bottom">
       <td class="no-underline"><a href="/users/${userId}/trips/${userTripId}">${city}</a></td>
       <td>${fave}</td></tr>`
-
   })
   tripsHtml += `</tbody></table>`
-return tripsHtml
-
-  //  btn.innerHTML = "Hide Trips"
-//  })
+  btn.innerHTML = "Hide Trips"
+  return tripsHtml
 }
 
 $(function() {
-//  txt = document.getElementById("my-trips");
   btn = document.getElementById("btn");
 
   $(".js-load-trips").on("click", function(event) {
     event.preventDefault()
-      if (btn.innerHTML === "See my trips") {
+      if (btn.innerHTML === "See My Trips") {
         loadUserTrips()
       } else if (btn.innerHTML === "Hide Trips"){
         $("div#my-trips").hide();
@@ -49,27 +44,12 @@ $(function() {
   })
 
   function loadUserTrips() {
-  //  debugger
     let userId = parseInt($(".js-load-trips").attr("data-user-id"))
     $.get(`/users/${userId}/trips.json`, function(data) {
-
       const user = new User(data);
-    let userTrips = user.formatTrips(userId)
-
+      let userTrips = user.formatTrips(userId)
       $("#my-trips").append(userTrips);
-      //let tripsList = "";
-    //  let something = data
-  //    userTrips.forEach(function(trip) {
-  //      let userTripId = trip.id;
-  //      let city = trip.city.name;
-      //  $("#my-trips").append(content);
-    //  $("#my-trips").append(`<a href="/users/${userId}/trips/${userTripId}">${city}</a><br>`);
-  //    btn.innerHTML = "Hide Trips"
-    //});
-  });
+    });
   }
-
-
-
 
 })
